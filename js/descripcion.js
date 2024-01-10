2335f983bfad2cfb0acdd1778d53cd5c9d2bfecc
CarlosPachxdd <156050949+CarlosPachxdd@users.noreply.github.com>
2024-01-09 20:37:18 -0600

    Add files via upload

---
+++ js/descripcion.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.servicio-item').tooltipster({
+        theme: 'tooltipster-borderless',
+        side: 'bottom',
+        touchDevices: true,
+        trigger: 'click',
+    });
+});
